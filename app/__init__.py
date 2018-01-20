807cf9b77abb3573c9ab97ece4492eb34a06e7fe
Maxie Corbin <max@looker.com>
2018-01-20 02:58:09 -0800

    fix some imports, create the scheduler, services, and slack objects

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,19 @@
-from flask import Flask, Config
+from flask import Flask
+from config import Config
+from slackclient import SlackClient
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from . import routes, models
+from . import routes
+from .models import Zendesk, Services
+
+scheduler = BackgroundScheduler()
+services = Services()
+slack = SlackClient(app.config['SLACK_TOKEN'])
+
+for service in services:
+    scheduler.add_job(service.check, 'interval', seconds=5, args=[slack])
+
+scheduler.start()
