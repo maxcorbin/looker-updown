af6e94bee15b8f9a31ed044dd48a3e5565cdeb32
Maxie Corbin <max@looker.com>
2018-01-20 10:53:05 -0800

    added some variables for debugging

--- updown.py
+++ updown.py
@@ -1,7 +1,9 @@
-from app import app
+from app import app, scheduler, services
 
 @app.shell_context_processor
 def make_shell_context():
     return {
-            'app': app
+            'app': app,
+            'scheduler': scheduler,
+            'services': services
             }
