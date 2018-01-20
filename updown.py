b4846b5261d41d5d321ba40f591a50452cb4be3b
Maxie Corbin <max@looker.com>
2018-01-19 21:03:32 -0800

    added appropriate shell context processor

--- updown.py
+++ updown.py
@@ -1,5 +1,8 @@
-from app import app
+from app import app, db
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'app': app}
+    return {
+            'app': app,
+            'db': db
+            }
