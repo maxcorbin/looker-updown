aa7bfd2fce45053841988b09cdfdb5c4555c9cb8
Maxie Corbin <max@looker.com>
2018-01-20 10:54:34 -0800

    added test service and finished Zendesk.check

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import random
 
 class Services(list):
     """This class initializes each service and puts them in an array to iterate over when scheduling jobs. Temporary solution until I work out some persistence.
@@ -7,6 +8,7 @@ class Services(list):
 
     def __init__(self):
         self.append(Zendesk())
+        self.append(TestService())
 
 class Zendesk(object):
     """This class represents the Zendesk service that is polled for its status.
@@ -17,7 +19,25 @@ class Zendesk(object):
         self.url = 'https://status.zendesk.com/api/internal/incidents.json'
 
     def check(self, slack):
-        slack.api_call('chat.postMessage',
-            channel='#general',
-            text=('*Zendesk Status Update: *' + requests.get(self.url).text)
-    	)
+        response = json.loads(requests.get(self.url))
+        incidents = response['current_incidents']
+        if len(incidents) > 0:
+            slack.api_call('chat.postMessage',
+                channel='#general',
+                text=('*Zendesk Status Update: *\n<!here> ' + str(incidents[0])))
+
+class TestService(object):
+
+    def rotate(l, n):
+        return l[n:] + l[:n]
+
+    def __init__(self):
+        self.dummies = [None]*19
+        self.dummies = self.dummies + ['Test service is down!']
+
+    def check(self, slack):
+        status = random.sample(self.dummies, 1)
+        if self.dummies[0]:
+            slack.api_call('chat.postMessage',
+                channel='#general',
+                text=('*Test Status Update: *\n<!here> ' + str(status)))
