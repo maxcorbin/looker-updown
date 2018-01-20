46470bf4989e05829c617c15760a83244693e718
Maxie Corbin <max@looker.com>
2018-01-20 02:57:11 -0800

    comment out twitter stuff

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-class Config(object):
+class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'its-a-secret-to-everyone'
-    SLACK_TOKEN = slack_token = os.environ['SLACK_TOKEN']
-    TWITTER_CONSUMER_KEY = os.environ["TWITTER_CONSUMER_KEY"]
-    TWITTER_CONSUMER_SECRET = os.environ["TWITTER_CONSUMER_SECRET"]
-    TWITTER_ACCESS_TOKEN = os.environ["TWITTER_ACCESS_TOKEN"]
-    TWITTER_ACCESS_TOKEN_SECRET = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+    SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
+    # TWITTER_CONSUMER_KEY = os.environ["TWITTER_CONSUMER_KEY"]
+    # TWITTER_CONSUMER_SECRET = os.environ["TWITTER_CONSUMER_SECRET"]
+    # TWITTER_ACCESS_TOKEN = os.environ["TWITTER_ACCESS_TOKEN"]
+    # TWITTER_ACCESS_TOKEN_SECRET = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
