6c5203d77185e915dd08bcbc44c6746aba6443cd
Maxie Corbin <max@looker.com>
2018-01-22 18:15:47 -0800

    add redis url to app config

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'its-a-secret-to-everyone'
     SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
+    REDIS_URL = os.environ.get('REDIS_URL')
     # TWITTER_CONSUMER_KEY = os.environ["TWITTER_CONSUMER_KEY"]
     # TWITTER_CONSUMER_SECRET = os.environ["TWITTER_CONSUMER_SECRET"]
     # TWITTER_ACCESS_TOKEN = os.environ["TWITTER_ACCESS_TOKEN"]
