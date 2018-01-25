83ca514485ec1288c8eab7737809946b5d9741cc
Maxie Corbin <max@looker.com>
2018-01-24 21:29:19 -0800

    added options for result_expires and redis_max_connections for celeryconfig

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,14 +1,14 @@
-
+import os
 
 imports = ('updown.tasks.checks')
-result_expires = 30
+result_expires = os.environ.get('CELERY_RESULT_EXPIRES')
 timezone = 'UTC'
 
 accept_content = ['json', 'msgpack', 'yaml']
 task_serializer = 'json'
 result_serializer = 'json'
 
-redis_max_connections = 20
+redis_max_connections = os.environ.get('CELERY_REDIS_MAX_CONNECTIONS')
 
 beat_schedule = {
     'zendesk.checks': {
