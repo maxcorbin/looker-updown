7ca0cb6379e1b0641a73a9bd8e712458888dcf5d
Maxie Corbin <max@looker.com>
2018-01-24 23:35:54 -0800

    adjusted config file for celery

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,24 +1,27 @@
-import os
+from os.environ import get
 from datetime import timedelta
 
-imports = ('updown.tasks.checks')
-result_expires = int(os.environ.get('CELERY_RESULT_EXPIRES')) or 15
-timezone = 'UTC'
+FIVE_MIN = timedelta(minutes=5)
+FIVE_SEC = timedelta(seconds=5)
 
+imports = ('updown.tasks.checks')
+timezone = 'UTC'
 accept_content = ['json', 'msgpack', 'yaml']
 task_serializer = 'json'
 result_serializer = 'json'
 
-redis_max_connections = int(os.environ.get('CELERY_REDIS_MAX_CONNECTIONS')) or 20
-broker_pool_limit = int(os.environ.get('CELERY_BROKER_POOL_LIMIT')) or 10
+result_expires = int(get('CELERY_RESULT_EXPIRES').strip() or FIVE_MIN)
+redis_max_connections = int(get('CELERY_REDIS_MAX_CONNECTIONS').strip() or 20)
+broker_pool_limit = int(get('CELERY_BROKER_POOL_LIMIT').strip() or 10)
+worker_concurrency = int(get('CELERY_WORKER_CONCURRENCY').strip() or 6)
 
 beat_schedule = {
     'zendesk.checks': {
         'task': 'updown.tasks.checks.check_zendesk',
-        'schedule': timedelta(seconds = 5.0)
+        'schedule': FIVE_SEC
     },
     'testservice.checks': {
         'task': 'updown.tasks.checks.check_testservice',
-        'schedule': timedelta(seconds = 5.0)
+        'schedule': FIVE_SEC
     }
 }
