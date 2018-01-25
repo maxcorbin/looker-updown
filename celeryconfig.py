6630c514da35473c508a2b33cf511d8340342b9c
Maxie Corbin <max@looker.com>
2018-01-24 22:42:06 -0800

    added timedelta cuz I think that it thought I meant 5 milliseconds?)

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 imports = ('updown.tasks.checks')
 result_expires = int(os.environ.get('CELERY_RESULT_EXPIRES')) or 15
@@ -14,10 +15,10 @@ broker_pool_limit = int(os.environ.get('CELERY_BROKER_POOL_LIMIT')) or 10
 beat_schedule = {
     'zendesk.checks': {
         'task': 'updown.tasks.checks.check_zendesk',
-        'schedule': 5.0
+        'schedule': timedelta(seconds = 5.0)
     },
     'testservice.checks': {
         'task': 'updown.tasks.checks.check_testservice',
-        'schedule': 5.0
+        'schedule': timedelta(seconds = 5.0)
     }
 }
