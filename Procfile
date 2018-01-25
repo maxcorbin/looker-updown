307e4eca947be25092dda47a07ee185e73b8daad
Maxie Corbin <max@looker.com>
2018-01-24 20:56:18 -0800

    removed singleton process

--- Procfile
+++ Procfile
@@ -1,4 +1,3 @@
-singleton: source bin/singleton.sh
 web: gunicorn updown:app
 clock: source bin/celerybeat.sh
 worker: source bin/celeryworker.sh
--- bin/singleton.sh
+++
@@ -1,9 +0,0 @@
-gunicorn updown:app
-
-celery -A updown.celery beat \
---schedule=/tmp/celery-schedule \
---pidfile=/tmp/celerybeat.pid \
---loglevel=INFO
-
-celery -A updown.celery worker \
---loglevel=INFO
